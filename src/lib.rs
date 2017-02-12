fc3b85f1dfe56b86fc612487eb6c31921280e24c
Martin Hafskjold Thoresen <martinhath@gmail.com>
2017-02-12 13:15:37 +0100

    Merge gradients, and save output to file.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#[macro_use] extern crate itertools;
 extern crate image;
 extern crate imageproc;
 use image::{GenericImage, Pixel};
@@ -8,6 +9,8 @@ struct Image {
     inner: image::DynamicImage,
 }
 
+type GradientBuffer = image::ImageBuffer<image::Luma<u16>, Vec<u16>>;
+
 impl Image {
     pub fn load_image(path: &Path) -> Image {
         Image {
@@ -15,21 +18,24 @@ impl Image {
         }
     }
 
-    fn gradient_magnitude(&self) {//-> image::ImageBuffer<image::Luma<u16>, Vec<u16>> {
+    fn gradient_magnitude(&self) -> GradientBuffer {
         let (red, green, blue) = decompose(&self.inner);
         let r_grad = imageproc::gradients::sobel_gradients(red.as_luma8().unwrap());
         let g_grad = imageproc::gradients::sobel_gradients(green.as_luma8().unwrap());
         let b_grad = imageproc::gradients::sobel_gradients(blue.as_luma8().unwrap());
-        // imageproc::gradients::sobel_gradients(&self.inner)
-        save_to_file(&r_grad, "red_grad.jpeg");
-        save_to_file(&g_grad, "green_grad.jpeg");
-        save_to_file(&b_grad, "blue_grad.jpeg");
+
+
+        let (w, h) = r_grad.dimensions();
+        let mut container = Vec::with_capacity((w * h) as usize);
+        for (r, g, b) in izip!(r_grad.pixels(), g_grad.pixels(), b_grad.pixels()) {
+            container.push(r[0] + g[0] + b[0]);
+        }
+        image::ImageBuffer::from_raw(w, h, container).unwrap()
     }
 }
 
-fn save_to_file(image: &image::ImageBuffer<image::Luma<u16>, Vec<u16>>, file_path: &'static str) {
-    // ugh
-    let u8_container = image.pixels().map(|a| a[0] as u8).collect::<Vec<_>>();
+fn save_to_file(image: &GradientBuffer, file_path: &'static str) {
+    let u8_container = image.pixels().map(|a| (a[0] / 2) as u8).collect::<Vec<_>>();
     let image: image::ImageBuffer<image::Luma<u8>, Vec<u8>> = image::ImageBuffer::from_raw(image.width(), image.height(), u8_container).unwrap();
     image.save(&Path::new(file_path)).unwrap();
 }
@@ -53,8 +59,8 @@ fn decompose(image: &image::DynamicImage) -> (image::DynamicImage, image::Dynami
 
 pub fn lib() {
     let image = Image::load_image(Path::new("sample-image.jpg"));
-    image.gradient_magnitude();
-
+    let grad = image.gradient_magnitude();
+    save_to_file(&grad, "gradient.jpeg");
 }
 
 
